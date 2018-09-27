f9835f360e28a50e2580cec02dba7cb7596650ee
Julien Hermine-Bromberg <julien.hermine-bromberg@epitech.eu>
2018-09-27 13:02:46 +0200

    initial commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//private InputField myInput;
+
+public class GameController : MonoBehaviour 
+{
+    public void GetInput(string username)
+    {
+        Debug.Log("You entered" + username);
+    }
+}
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour 
+{
+    public string playGameLevel;
+    public string highSoreScreen;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(playGameLevel);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    public void HighScore()
+    {
+        SceneManager.LoadScene(highSoreScreen);
+    }
+}
